29a6a7725b541b4c026c745078fe6656c111ea81
Suldem <suldemka1@gmail.com>
2022-07-08 10:08:02 +0300

    slider added

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,56 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/header'
 import News from './news'
-import styles from '../styles/Home.module.css'
-import MobileHeader from '../components/mobile-header'
 import Service from '../components/govservice'
 import Resourse from '../components/govresourses'
-import Footer from '../components/footer'
+import { Carousel } from 'react-bootstrap'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
+
+      <Carousel>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="/newspic.jpg"
+            alt="First slide"
+            width={1100}
+            height={600}
+          />
+          <Carousel.Caption>
+            <h3>Заголовок новости №1</h3>
+            <p>Текст новости на 1 строку примерно 100 символов</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="/newspic.jpg"
+            alt="Second slide"
+            width={1100}
+            height={600}
+          />
+
+          <Carousel.Caption>
+            <h3>Заголовок новости №2</h3>
+            <p>Текст новости на 1 строку примерно 100 символов</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="/newspic.jpg"
+            alt="Third slide"
+            width={1100}
+            height={600}
+          />
+
+          <Carousel.Caption>
+            <h3>Заголовок новости №3</h3>
+            <p>Текст новости на 1 строку примерно 100 символов</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       <News />
       <Service />
       <Resourse />
