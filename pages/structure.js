13318e90052d247523631cad8903fc12809f1a09
Suldem <suldemka1@gmail.com>
2022-07-08 10:23:46 +0300

    bug

--- pages/structure.js
+++ pages/structure.js
@@ -3,95 +3,95 @@ import Image from "next/image";
 
 export default function Structure() {
 
-  let management = [
-    // {
-    //   surname: 'Сат',
-    //   name: 'Айдыс',
-    //   patronymic: 'Александрович',
-    //   position: 'Министр экономического развития и промышленности Республики Тыва'
-    // },
+  // let management = [
+  //   // {
+  //   //   surname: 'Сат',
+  //   //   name: 'Айдыс',
+  //   //   patronymic: 'Александрович',
+  //   //   position: 'Министр экономического развития и промышленности Республики Тыва'
+  //   // },
 
-    // {
-    //   surname: 'Сат',
-    //   name: 'Айдыс',
-    //   patronymic: 'Александрович',
-    //   position: 'Министр экономического развития и промышленности Республики Тыва'
-    // },
+  //   // {
+  //   //   surname: 'Сат',
+  //   //   name: 'Айдыс',
+  //   //   patronymic: 'Александрович',
+  //   //   position: 'Министр экономического развития и промышленности Республики Тыва'
+  //   // },
 
-    {
-      surname: 'Ховалыг',
-      name: 'Снежана',
-      patronymic: 'Кушкаш-ооловна',
-      position: 'Первый заместитель министра',
-      email: 'mineconom@rtyva.ru',
-      phone: '+7 (39422) 9-76-62'
-    },
+  //   {
+  //     surname: 'Ховалыг',
+  //     name: 'Снежана',
+  //     patronymic: 'Кушкаш-ооловна',
+  //     position: 'Первый заместитель министра',
+  //     email: 'mineconom@rtyva.ru',
+  //     phone: '+7 (39422) 9-76-62'
+  //   },
 
-    {
-      surname: 'Конгар',
-      name: 'Анзатмаа',
-      patronymic: 'Аяс-Кызы',
-      position: 'Заместитель министра - начальник департамента по развитию промышленности и инвестиционной политике',
-      email: 'mineconom@rtyva.ru',
-      phone: '+7 (39422) 9-76-62'
-    },
+  //   {
+  //     surname: 'Конгар',
+  //     name: 'Анзатмаа',
+  //     patronymic: 'Аяс-Кызы',
+  //     position: 'Заместитель министра - начальник департамента по развитию промышленности и инвестиционной политике',
+  //     email: 'mineconom@rtyva.ru',
+  //     phone: '+7 (39422) 9-76-62'
+  //   },
 
-    {
-      surname: 'Таспанчик',
-      name: 'Темир',
-      patronymic: 'Чаш-оолович',
-      position: 'Заместитель министра',
-      email: 'mineconom@rtyva.ru',
-      phone: '+7 (39422) 9-76-62'
-    },
+  //   {
+  //     surname: 'Таспанчик',
+  //     name: 'Темир',
+  //     patronymic: 'Чаш-оолович',
+  //     position: 'Заместитель министра',
+  //     email: 'mineconom@rtyva.ru',
+  //     phone: '+7 (39422) 9-76-62'
+  //   },
 
-    {
-      surname: 'Фамилия',
-      name: 'Имя',
-      patronymic: 'Отчество',
-      position: 'Специалист',
-      email: 'somemail@mail.ru',
-      phone: '+7 (39422) 2-25-45'
-    },
+  //   {
+  //     surname: 'Фамилия',
+  //     name: 'Имя',
+  //     patronymic: 'Отчество',
+  //     position: 'Специалист',
+  //     email: 'somemail@mail.ru',
+  //     phone: '+7 (39422) 2-25-45'
+  //   },
 
-    {
-      surname: 'Фамилия',
-      name: 'Имя',
-      patronymic: 'Отчество',
-      position: 'Специалист',
-      email: 'somemail@mail.ru',
-      phone: '+7 (39422) 2-25-45'
-    },
+  //   {
+  //     surname: 'Фамилия',
+  //     name: 'Имя',
+  //     patronymic: 'Отчество',
+  //     position: 'Специалист',
+  //     email: 'somemail@mail.ru',
+  //     phone: '+7 (39422) 2-25-45'
+  //   },
 
-    {
-      surname: 'Фамилия',
-      name: 'Имя',
-      patronymic: 'Отчество',
-      position: 'Специалист',
-      email: 'somemail@mail.ru',
-      phone: '+7 (39422) 2-25-45'
-    },
+  //   {
+  //     surname: 'Фамилия',
+  //     name: 'Имя',
+  //     patronymic: 'Отчество',
+  //     position: 'Специалист',
+  //     email: 'somemail@mail.ru',
+  //     phone: '+7 (39422) 2-25-45'
+  //   },
 
-    {
-      surname: 'Фамилия',
-      name: 'Имя',
-      patronymic: 'Отчество',
-      position: 'Специалист',
-      email: 'somemail@mail.ru',
-      phone: '+7 (39422) 2-25-45'
-    },
+  //   {
+  //     surname: 'Фамилия',
+  //     name: 'Имя',
+  //     patronymic: 'Отчество',
+  //     position: 'Специалист',
+  //     email: 'somemail@mail.ru',
+  //     phone: '+7 (39422) 2-25-45'
+  //   },
     
-    {
-      surname: 'Фамилия',
-      name: 'Имя',
-      patronymic: 'Отчество',
-      position: 'Специалист',
-      email: 'somemail@mail.ru',
-      phone: '+7 (39422) 2-25-45'
-    },
-  ]
+  //   {
+  //     surname: 'Фамилия',
+  //     name: 'Имя',
+  //     patronymic: 'Отчество',
+  //     position: 'Специалист',
+  //     email: 'somemail@mail.ru',
+  //     phone: '+7 (39422) 2-25-45'
+  //   },
+  // ]
 
-  const show = management.map(item => {
+  const show = management.map((item) => {
     return <Card className="card">
       <Card.Header className="card-header">
         <Card.Title className="card-title">{item.position}</Card.Title>
