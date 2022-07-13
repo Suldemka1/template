e8ebbf78c756c7ef3eefd08955cbea8f79901e8e
Suldem <suldemka1@gmail.com>
2022-07-13 06:30:31 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -8,55 +8,6 @@ export default function Home() {
     <>
 <<<<<<< HEAD
       <NewsCarousel/>
-=======
-      <div className='slider'>
-        <Carousel>
-          <Carousel.Item>
-            <Image
-              className="d-block w-100"
-              src="/newspic.jpg"
-              alt="First slide"
-              width={1300}
-              height={650}
-            />
-            <Carousel.Caption>
-              <h3>Заголовок новости №1</h3>
-              <p>Текст новости на 1 строку примерно 100 символов</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              className="d-block w-100"
-              src="/newspic.jpg"
-              alt="Second slide"
-              width={1300}
-              height={650}
-            />
-
-            <Carousel.Caption>
-              <h3>Заголовок новости №2</h3>
-              <p>Текст новости на 1 строку примерно 100 символов</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              className="d-block w-100"
-              src="/newspic.jpg"
-              alt="Third slide"
-              width={1300}
-              height={650}
-            />
-
-            <Carousel.Caption>
-              <h3>Заголовок новости №3</h3>
-              <p>Текст новости на 1 строку примерно 100 символов</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-
-
->>>>>>> 8c2b25f843d926b05455c3753f1c03ffa86dc110
       <Service />
       <Resourse />
     </>
