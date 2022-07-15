110c3b354a606eb803fff8726b7087e32b27cd32
Suldem <suldemka1@gmail.com>
2022-07-15 13:49:59 +0300

    commit

--- components/govservice.js
+++ components/govservice.js
@@ -9,7 +9,7 @@ export default function Resourse() {
         <h2>Государственные услуги</h2>
         <div className='cards'>
           <div className='card'>
-            <Link href="/"><a><div>Название государсвенной услуги со ссылкой на неё</div><Image alt='some' src='/' width={80} height={80}/></a></Link>
+            <Link href="/"><a><div>Регистрация транспортного средства предприятия</div><Image alt='some' src='/' width={50} height={50} className='image'/></a></Link>
           </div>
           <div className='card'>
             <Link href="/"><a><div>Название государсвенной услуги со ссылкой на неё</div><Image alt='some' src='/' width={60} height={60}/></a></Link>
