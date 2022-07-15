a7de0a8faf6058207ed69146e2df3112703d95a9
Suldem <suldemka1@gmail.com>
2022-07-15 14:41:10 +0300

    commit

--- pages/sendrequest.js
+++ pages/sendrequest.js
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
 import { Form, FloatingLabel, FormGroup, FormCheck, Button } from "react-bootstrap";
 import { PageName } from "../components/PageName/PageName";
+import axios from 'axios'
 
 export default function SendRequest() {
+  const [snp, setSnp] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [feedback, setFeedback] = useState('')
+  const [offerCheck, setOfferCheck] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+
+
+  const handleSubmit = () => {
+
+      axios.post('url',
+        {
+          snp: snp,
+          phone: phone,
+          email: email,
+          feedback: feedback
+        })
+
+    console.log(snp)
+  }
+
+  const handleOfferCheckChange = (e) => {
+
+    if (offerCheck == false) {
+      setOfferCheck(true)
+      setButtonDisabled(false)
+      console.log('принято')
+    }
+    else {
+      setOfferCheck(false)
+      setButtonDisabled(true)
+      console.log('не принято')
+    }
+
+    console.log(offerCheck)
+  }
+
+  useEffect(() => {
+  })
+
+
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <h2></h2>
       <PageName title='Прием обращений граждан' />
       <FormGroup style={{ width: '30rem', margin: "auto" }}>
@@ -12,32 +56,34 @@ export default function SendRequest() {
           controlId="floatingInput"
           label="Фамилия Имя Отчество"
           className="mb-3">
-          <Form.Control type="email" value={''} onChange={e => setSender(e.target.value)} placeholder="Фамилия Имя Отчество" />
+          <Form.Control value={snp} onChange={(e) => setSnp(e.target.value)} placeholder="Фамилия Имя Отчество" />
         </FloatingLabel>
         <FloatingLabel
           controlId="floatingInput"
           label="Номер телефона"
           className="mb-3">
-          <Form.Control type="phone" value={''} onChange={e => setSender(e.target.value)} placeholder="Номер телефона" />
+          <Form.Control type="phone" value={phone} onChange={e => setPhone(e.target.value)} placeholder="Номер телефона" />
         </FloatingLabel>
         <FloatingLabel
           controlId="floatingInput"
           label="Адрес электронной почты"
           className="mb-3">
-          <Form.Control type="email" value={''} onChange={e => setSender(e.target.value)} placeholder="Адрес электронной почты" />
+          <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Адрес электронной почты" />
         </FloatingLabel>
 
         <FormGroup>
-          <Form.Control as="textarea" value={''} onChange={e => setFeedback(e.target.value)} rows={4} placeholder="Введите ваше сообщение" style={{ resize: 'none' }} />
+          <Form.Control as="textarea" value={feedback} onChange={e => setFeedback(e.target.value)} rows={4} placeholder="Введите ваше сообщение" style={{ resize: 'none' }} />
         </FormGroup>
         <FormGroup>
           <FormCheck
             type="switch"
             id="custom-switch"
             label="Я ознакомлен с Политикой обработки персональных данных и принимаю публичную Оферту"
+            value={offerCheck}
+            onChange={handleOfferCheckChange}
           ></FormCheck>
         </FormGroup>
-        <Button type="submit">Отправить</Button>
+        <Button type="submit" disabled={buttonDisabled} >Отправить</Button>
       </FormGroup>
     </Form>
   )
