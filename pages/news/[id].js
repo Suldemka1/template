3fa0baf915c98884b741836387aa7c7a1310c858
Suldem <suldemka1@gmail.com>
2022-07-19 18:26:27 +0700

    commit #9

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import PostInfo from "../../components/PostInfo";
 
-export const getStaticPaths = async () => {
-  const response = await fetch(`http://${process.env.APIpath}/api/news/`);
-  const data = await response.json();
+// export const getStaticPaths = async () => {
+//   const response = await fetch(`http://${process.env.APIpath}/api/news/`);
+//   const data = await response.json();
 
-  const paths = data.response.map(({ id }) => ({
-    params: { id: id.toString() }
-  }));
+//   const paths = data.response.map(({ id }) => ({
+//     params: { id: id.toString() }
+//   }));
 
-  return {
-    paths,
-    fallback: false,
-  }
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// };
 
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
   const { id } = context.params;
   const response = await fetch(`http://${process.env.APIpath}/api/news/${id}`);
   const data = await response.json();
